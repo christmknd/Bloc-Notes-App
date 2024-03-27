ffbc386d109d0ba80a5a022382f3d3399bbbdbb2
Christ MTAE <christ@mtae.local>
2024-03-27 17:07:18 +0100

    Finish User Controller & Service

--- bn-backend/src/users/users.controller.ts
+++ bn-backend/src/users/users.controller.ts
@@ -1,34 +1,108 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+  NotFoundException,
+  BadRequestException,
+} from '@nestjs/common';
 import { UsersService } from './users.service';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import {
+  ApiBadRequestResponse,
+  ApiBody,
+  ApiNotFoundResponse,
+  ApiOperation,
+  ApiParam,
+  ApiResponse,
+} from '@nestjs/swagger';
+import { User } from './entities/user.entity';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Create a new task' })
+  @ApiBody({ type: CreateUserDto })
+  @ApiResponse({
+    status: 201,
+    description: 'Task created successfully',
+    type: User,
+  })
+  @ApiBadRequestResponse({ description: 'Task cannot be registrated' })
   @Post()
-  create(@Body() createUserDto: CreateUserDto) {
-    return this.usersService.create(createUserDto);
+  create(@Body() CreateUserDto: CreateUserDto) {
+    try {
+      return this.usersService.createUser(CreateUserDto);
+    } catch {
+      throw new BadRequestException('Task cannot be registrated');
+    }
   }
 
+  @ApiOperation({ summary: 'Get all tasks' })
+  @ApiResponse({
+    status: 200,
+    description: 'Return all tasks',
+    type: User,
+  })
+  @ApiNotFoundResponse({ description: 'No tasks were found' })
   @Get()
   findAll() {
-    return this.usersService.findAll();
+    try {
+      return this.usersService.findAllUser();
+    } catch {
+      throw new NotFoundException('No tasks were found');
+    }
   }
 
+  @ApiOperation({ summary: 'Get task by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiResponse({
+    status: 200,
+    description: 'Return task by ID',
+    type: User,
+  })
+  @ApiNotFoundResponse({ description: 'Task not found' })
   @Get(':id')
-  findOne(@Param('id') id: string) {
-    return this.usersService.findOne(+id);
+  findTaskById(@Param('id') id: number) {
+    try {
+      return this.usersService.findUserById(+id);
+    } catch {
+      throw new NotFoundException('Task not found');
+    }
   }
 
+  @ApiOperation({ summary: 'Update task by ID' })
+  @ApiParam({ name: 'id', type: Number })
+  @ApiBody({ type: UpdateUserDto })
+  @ApiResponse({
+    status: 200,
+    description: 'Task updated successfully',
+    type: User,
+  })
+  @ApiNotFoundResponse({
+    description: 'Task not found : Task cannot be updated',
+  })
   @Patch(':id')
-  update(@Param('id') id: string, @Body() updateUserDto: UpdateUserDto) {
-    return this.usersService.update(+id, updateUserDto);
+  update(@Param('id') id: number, @Body() UpdateUserDto: UpdateUserDto) {
+    try {
+      return this.usersService.updateUser(+id, UpdateUserDto);
+    } catch {
+      throw new NotFoundException('Task not found : Task cannot be updated');
+    }
   }
 
   @Delete(':id')
-  remove(@Param('id') id: string) {
-    return this.usersService.remove(+id);
+  @ApiOperation({ summary: 'Delete task by ID' })
+  delete(@Param('id') id: number) {
+    try {
+      return this.usersService.deleteUser(+id);
+    } catch {
+      throw new NotFoundException('Task not found : Task cannot be deleted');
+    }
   }
 }
--- bn-backend/src/users/users.service.ts
+++ bn-backend/src/users/users.service.ts
@@ -1,26 +1,52 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UsersService {
-  create(createUserDto: CreateUserDto) {
-    return 'This action adds a new user';
+  constructor(
+    @InjectRepository(User)
+    private userRepository: Repository<User>,
+  ) {}
+
+  async createUser(createUserDto: CreateUserDto): Promise<User> {
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    const user = this.userRepository.create(CreateUserDto);
+    // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+    // @ts-ignore
+    return this.userRepository.save(user);
   }
 
-  findAll() {
-    return `This action returns all users`;
+  async findAllUser(): Promise<User[]> {
+    return this.userRepository.find();
   }
 
-  findOne(id: number) {
-    return `This action returns a #${id} user`;
+  async findUserById(id: number): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+    return user;
   }
 
-  update(id: number, updateUserDto: UpdateUserDto) {
-    return `This action updates a #${id} user`;
+  async updateUser(id: number, updateUserDto: UpdateUserDto): Promise<User> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new NotFoundException(`Task with ID ${id} not found`);
+    }
+    Object.assign(user, updateUserDto);
+    return this.userRepository.save(user);
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} user`;
+  async deleteUser(id: number): Promise<void> {
+    const user = await this.userRepository.findOneBy({ id: id });
+    if (!user) {
+      throw new NotFoundException(`Task with id ${id} not found`);
+    }
+    await this.userRepository.delete(user);
   }
 }
